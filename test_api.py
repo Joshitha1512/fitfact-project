ad1557dcefd80f57b6d78708630c81002ba6d21f
Joshitha1512 <joshimugi15@gmail.com>
2025-10-01 00:11:22 +0530

    Completed backend placeholder setup

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/verify_claim"
+url = "https://fitfact-backend.onrender.com/verify_claim"
 data = {"claim": "100 sit-ups burn belly fat"}
 
 response = requests.post(url, json=data)
